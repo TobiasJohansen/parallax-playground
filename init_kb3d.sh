9f1b703e5553442fccd038ffcbf744a76337217f
TobiasJohansen <tobjoh1994@gmail.com>
2020-07-19 22:35:40 +0200

    changes to requirements and init

--- init_kb3d.sh
+++ init_kb3d.sh
@@ -1,11 +1,11 @@
 # Download the repo from Github
 git clone https://github.com/sniklaus/3d-ken-burns.git kb3d
 
+# Install requirements
+pip install -r requirements_kb3d.txt
+
 # Move into the downloaded repository
 cd kb3d
-
-# Install requirements
-pip install -r requirements.txt
 
 # Create a new directory to store the videos
 mkdir videos
